05c8baa3d7b9a8534e9e2967b9897ecc9c29289c
RICHE AMELIE p1607858 <amelie.riche@etu.univ-lyon1.fr>
2019-12-06 04:02:41 +0100

    Debut chasse

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,12 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/{_locale}")
  */
 class MainController extends AbstractController
 {
+    public function __construct()
+    {
+        $cookie = new Cookie('secret_state', '0', strtotime('tomorrow'));
+    }
+
     /**
      * @Route("/home", name="home")
      */
@@ -17,6 +24,64 @@ class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/question/1", name="secret_logement")
+     */
+    public function pageLogement()
+    {
+        return $this->render('main/Logement.html.twig');
+    }
+
+    /**
+     * @Route("/question/2", name="secret_aideLogement")
+     */
+    public function pageAideLogement()
+    {
+        return $this->render('main/aideLogement.html.twig');
+    }
+
+    /**
+     * @Route("/question/3", name="secret_velov")
+     */
+    public function pagevelov()
+    {
+        return $this->render('main/velov.html.twig');
+    }
+
+    /**
+     * @Route("/question/4", name="secret_amlRecrutement")
+     */
+    public function pageAml()
+    {
+        return $this->render('main/amlRecrutement.html.twig');
+    }
+
+    /**
+     * @Route("/question/5", name="secret_planningFam")
+     */
+    public function pagePlanningFam()
+    {
+        return $this->render('main/planningFam.html.twig');
+    }
+
+    /**
+     * @Route("/question/6", name="secret_stage")
+     */
+    public function pageStage()
+    {
+        return $this->render('main/stage.html.twig');
+    }
+
+    /**
+     * @Route("/secret", name="secret")
+     */
+    public function secret()
+    {
+        return $this->render('main/secret.html.twig', [
+            'secret_state' => 0,
         ]);
     }
 }
